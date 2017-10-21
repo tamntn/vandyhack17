92ec505867918baf6fcb6ce470a56ee3b8972c1d
sdhoju <sdhoju@go.olemiss.edu>
2017-10-21 16:28:45 -0500

    s

--- PythonFlask/main.py
+++ PythonFlask/main.py
@@ -7,18 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	data = {"lat": 37.775, "lng": -56.434}
-	return render_template('crime_foretell.html', result=data)
-
-@app.route('/maps')
-def maps(data=None):
-	data = {"lat": 37.775, "lng": -56.434}
-	return render_template('maps.html',result=data)
-
-@app.route('/test')
-def test(data=None):
-	data = {"lat": 37.775, "lng": -122.434}
-	return render_template('test.html',result=json.dumps(data))
+	
+	return "hellow world"
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
