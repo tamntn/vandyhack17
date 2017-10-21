aa60c649bee8b2adf1aad3a72aa054bdd193dbc8
Aishwant Ghimire <aghimire@go.olemiss.edu>
2017-10-21 03:34:14 -0500

    Sample Working Flask

---
+++ PythonFlask/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for
+import json
+
+app = Flask(__name__)
+
+
+# @app.route("/")
+# def hello():
+# 	return render_template('crime_foretell.html')
+
+@app.route('/maps')
+def maps(data=None):
+	data = {"lat": 37.775, "lng": -56.434}
+	return render_template('maps.html',result=data)
+
+@app.route('/')
+def test(data=None):
+	data = {"lat": 37.775, "lng": -122.434}
+	return render_template('test.html',result=json.dumps(data))
