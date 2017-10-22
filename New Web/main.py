8f8dd20ae3096d1d1c3c44ae338bad04a7861b4a
Tam Nguyen <tam.nguyen9779@gmail.com>
2017-10-22 07:41:34 -0500

    First Draft

--- New Web/main.py
+++ New Web/main.py
@@ -1,16 +1,56 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import json
+from model_func import *
+import operator
+import datetime
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello():
 	return render_template('dashboard.html')
 
 @app.route("/locationPrediction")
+@app.route("/locationPrediction", methods=['POST', 'GET'])
 def locationPrediction():
-	return render_template('locationPrediction.html')
+	if request.method == 'POST':
+		month = request.form['month']
+		time = request.form['time']
+		day = request.form['day']
+		dct_zipProb = prob_zip(month, time, day)
+		zipAndProb = prob_zip_MClearning(dct_zipProb)
+		return render_template('locationPrediction.html', data=zipAndProb)
+	else:
+		now = datetime.datetime.now()
+		month = str(now.month)
+		time = str(now.hour)
+		day = datetime.datetime.today().weekday()
+		if day==0:
+			day = "Monday"
+		elif day==1:
+			day = "Tuesday"
+		elif day==2:
+			day = "Wednesday"
+		elif day==3:
+			day = "Thursday"
+		elif day==4:
+			day = "Friday"
+		elif day==5:
+			day = "Saturday"
+		else:
+			day = "Sunday"
+		dct_zipProb = prob_zip(month, time, day)
+		zipAndProb = prob_zip_MClearning(dct_zipProb)
+		return render_template('locationPrediction.html', data=zipAndProb)
+
+def prob_zip_MClearning(dct_zipProb):
+	sorted_dct = sorted(dct_zipProb.items(), key=operator.itemgetter(1))
+	sorted_dct = dict(sorted_dct)
+	all_lat_lng = []
+	for k,v in sorted_dct.items():
+		all_lat_lng.append(k)
+		all_lat_lng.append(np.float32(v))
+	return all_lat_lng
 
 @app.route("/typePrediction")
 def typePrediction():
